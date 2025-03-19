fa160c43420cb993acaa2e21fa15eec87f5d1696
kevin <kevinsanchezduran4@gmail.com>
2025-03-19 08:03:43 -0600

    Agregar archivos de proyecto.

---
+++ Kevin20250319AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kevin20250319AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
