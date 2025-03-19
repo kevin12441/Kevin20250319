853edb389c72d02449163336972d21f5538c4209
Oscar Eliseo Torres Vasquez <oscar2004torres@outlook.es>
2025-03-19 11:49:52 -0600

    validaciones

--- Kevin20250319AppWebMVC/Models/Product.cs
+++ Kevin20250319AppWebMVC/Models/Product.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Kevin20250319AppWebMVC.Models;
 
 public partial class Product
 {
     public int ProductId { get; set; }
-
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string ProductName { get; set; } = null!;
 
     public string? Description { get; set; }
-
+    [Required(ErrorMessage = "El precio es obligatorio.")]
     public decimal Price { get; set; }
 
     public int? CategoryId { get; set; }
--- Kevin20250319AppWebMVC/Models/User.cs
+++ Kevin20250319AppWebMVC/Models/User.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Kevin20250319AppWebMVC.Models;
 
 public partial class User
 {
     public int UserId { get; set; }
-
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string Username { get; set; } = null!;
+    [Required(ErrorMessage = "El Email es obligatorio.")]
+    [EmailAddress(ErrorMessage = "El correo electrónico no tiene un formato válido.")]
 
     public string Email { get; set; } = null!;
-
+    [Required(ErrorMessage = "El password es obligatorio.")]
+    [DataType(DataType.Password)]
+    [StringLength(40, MinimumLength = 5, ErrorMessage = "El passowrd debe tener entre 5 y 50 caracteres.")]
     public string PasswordHash { get; set; } = null!;
 
     public string Role { get; set; } = null!;
